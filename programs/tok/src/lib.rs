2b9339d1b168b19d229e19eb65dc1b6d14b161f7
DeVxMe <bimalcgalise123@gmail.com>
2025-09-13 11:21:16 +0545

    Chore : Some Other

---
+++ programs/tok/src/lib.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+
+// This is your program's public key and it will update
+// automatically when you build the project.
+declare_id!("ETYD4aem41fzbewZr3FgHEWg1Hn1j2VZwq47HAG2kbyJ");
+
+#[program]
+mod journal {
+    use super::*;
+}
